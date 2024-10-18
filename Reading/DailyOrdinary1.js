4dcc6bdcb3d006719a500406f852d01117143d98
anrevietson <143390131+anrevietson@users.noreply.github.com>
2024-10-18 07:50:30 +0700

    Update DailyOrdinary1.js

--- Reading/DailyOrdinary1.js
+++ Reading/DailyOrdinary1.js
@@ -2724,7 +2724,7 @@ window.bibledailyOrdinary1Readings =
       "end": "Ðó là lời Chúa"
     }
   },
-  "5163": {
+  "5164": {
     "firstReading": {
       "excerpt": "(Năm I) Xh 19, 1-2. 9-11. 16-20b",
       "info": "“Chúa ngự xuống trên núi Sinai trước mặt toàn dân”.",
@@ -2755,7 +2755,7 @@ window.bibledailyOrdinary1Readings =
       "end": "Ðó là lời Chúa"
     }
   },
-  "5164": {
+  "5163": {
     "firstReading": {
       "excerpt": "(Năm I) Xh 16, 1-5. 9-15",
       "info": "“Ta sẽ cho mưa bánh từ trời rơi xuống”.",
